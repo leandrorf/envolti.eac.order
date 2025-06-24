1c568d99482d38b8af1908dc7a32d69cb8d332b4
Leandro Rodrigues <leandro.rodrigues.fernandes@live.com>
2025-06-24 19:22:09 -0300

    Identificando porque a mensagem no grafana está se repetindo! - 5

---
+++ src/core/application/envolti.lib.order.application/NoMessageTemplateJsonFormatter.cs
@@ -0,0 +1,32 @@
+﻿using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Formatting.Json;
+using Serilog.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace envolti.lib.order.application
+{
+    public class NoMessageTemplateJsonFormatter : ITextFormatter
+    {
+        private readonly JsonFormatter _baseFormatter = new JsonFormatter( );
+
+        public void Format( LogEvent logEvent, TextWriter output )
+        {
+            // Cria novo LogEvent sem MessageTemplate
+            var logEventWithoutTemplate = new LogEvent(
+                logEvent.Timestamp,
+                logEvent.Level,
+                logEvent.Exception,
+                // Template vazio, pois não será serializado
+                new MessageTemplate( "", new List<MessageTemplateToken>( ) ),
+                logEvent.Properties.Select( kv => new LogEventProperty( kv.Key, kv.Value ) )
+            );
+
+            _baseFormatter.Format( logEventWithoutTemplate, output );
+        }
+    }
+}
